a2ae62651e0ab163177465c494ffab7f72e028d6
Dmytruk Serhii <dmitruksl92@gmail.com>
2024-12-06 14:57:04 +0200

    Refactored code

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/JellyObjCanvasView.vue')
+      component: () => import('../views/canvas/JellyObjCanvasView.vue')
     },
     {
       path: '/jelly-simple-img',
@@ -25,42 +25,42 @@ const router = createRouter({
     {
       path: '/webgl-triangle',
       name: 'webgl-triangle',
-      component: () => import('../views/WebGlTriangleView.vue')
+      component: () => import('../views/webgl/WebGlTriangleView.vue')
     },
     {
       path: '/threejs-triangle',
       name: 'threejs-triangle',
-      component: () => import('../views/ThreeTriangleView.vue')
+      component: () => import('../views/treejs/ThreeTriangleView.vue')
     },
     {
       path: '/webgl-2-triangle',
       name: 'webgl-2-triangle',
-      component: () => import('../views/WebGlTriangle2View.vue')
+      component: () => import('../views/webgl/WebGlTriangle2View.vue')
     },
     {
       path: '/webgl-cube',
       name: 'webgl-cube',
-      component: () => import('../views/WebGlCubeView.vue')
+      component: () => import('../views/webgl/WebGlCubeView.vue')
     },
     {
       path: '/webgl-cube-texture',
       name: 'webgl-cube-texture',
-      component: () => import('../views/WebGlCubeTextureView.vue')
+      component: () => import('../views/webgl/WebGlCubeTextureView.vue')
     },
     {
       path: '/webgl-pyramid-x-trans',
       name: 'webgl-pyramid',
-      component: () => import('../views/WebGlPyramidXTransView.vue')
+      component: () => import('../views/webgl/WebGlPyramidXTransView.vue')
     },
     {
       path: '/jelly-simple-svg',
       name: 'jelly-simple-svg',
-      component: () => import('../views/JellySVGCanvasView.vue')
+      component: () => import('../views/canvas/JellySVGCanvasView.vue')
     },
     {
       path: '/treejs-pyramid-x-trans',
       name: 'treejs-pyramid',
-      component: () => import('../views/TreeJsPyramidXTransView.vue')
+      component: () => import('../views/treejs/TreeJsPyramidXTransView.vue')
     }
   ]
 })
